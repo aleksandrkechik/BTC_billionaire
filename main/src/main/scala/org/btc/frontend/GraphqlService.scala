44478ae056cdb1ae54fac141f8ab9f3ac7c4b962
aleksandrkechik <49484710+aleksandrkechik@users.noreply.github.com>
2022-04-01 14:43:30 +0300

    01.04

---
+++ main/src/main/scala/org/btc/frontend/GraphqlService.scala
@@ -0,0 +1,59 @@
+package org.btc.frontend
+
+import akka.actor.ActorRef
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.util.Timeout
+import org.btc.frontend.graphql.Data
+import org.btc.frontend.graphql.Schema
+import org.slf4j.LoggerFactory
+import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
+import sangria.http.akka.circe.CirceHttpSupport
+import sangria.slowlog.SlowLog
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
+class GraphqlService()(implicit executionContext: ExecutionContext) extends
+  CirceHttpSupport {
+  val log = LoggerFactory.getLogger(this.getClass.getName)
+  implicit val timeout: Timeout = Timeout(2 seconds)
+
+  import sangria.marshalling.circe._
+
+//  override
+  def route: Route = graphqlRoute
+
+  val schema = Schema.schema
+
+  val graphqlRoute: Route =
+    optionalHeaderValueByName("X-Apollo-Tracing") { tracing =>
+      path("graphql") {
+        graphQLPlayground ~
+            prepareGraphQLRequest {
+              case Success(req) =>
+                val middleware = if (tracing.isDefined) SlowLog.apolloTracing :: Nil else Nil
+                //              val deferredResolver = DeferredResolver.fetchers(SchemaDefinition.characters)
+                val graphQLResponse = Executor.execute(
+                  schema = schema,
+                  queryAst = req.query,
+                  Data.SecureContext(),
+                  variables = req.variables,
+                  operationName = req.operationName,
+                  middleware = middleware,
+                  //                deferredResolver = deferredResolver
+                ).map(OK -> _)
+                  .recover {
+                    case error: QueryAnalysisError => BadRequest -> error.resolveError
+                    case error: ErrorWithResolver => InternalServerError -> error.resolveError
+                  }
+                complete(graphQLResponse)
+              case Failure(preparationError) => complete(BadRequest, formatError(preparationError))
+            }
+      }
+    }
+}
+
+
